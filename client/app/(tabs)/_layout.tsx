4509d1152598063ee7504f226f312409f344e411
Mariem Chebbi <chebbimariem8888@gmail.com>
2024-06-30 21:56:09 +0100

    test01

--- client/app/(tabs)/_layout.tsx
+++ client/app/(tabs)/_layout.tsx
@@ -10,6 +10,7 @@ export default function TabLayout() {
 
   return (
     <Tabs
+    //test
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
