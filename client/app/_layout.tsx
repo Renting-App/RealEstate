23585c460cf6e3d58de700dd74ddd4291a40cc2b
Mariem Chebbi <chebbimariem8888@gmail.com>
2024-07-18 19:50:49 +0100

    structure

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Welcome from './Welcome';
 import Signin from './SignIn';
@@ -9,12 +8,13 @@ import AdminPage from './adminPage';
 import PostProperty from './PostProperty';
 import FAQ from './FAQ';
 import Maps from './Maps';
-import Favourite from './Favorite';
-import FilterComponent from './FilterComponent';
+// import Favourite from './Favorite';
 import RequestTour from './RequestTour';
 import AboutUs from './AboutUs';
 import PropertyDetails from './PropertyDetails';
 // import FilteredDataComponent from './FilteredData';
+// import FilterComponent from './FilterComponent';
+
 
 export type RootStackParamList = {
   Welcome: undefined;
@@ -80,15 +80,17 @@ export default function App() {
         {/* <Stack.Screen
           name="FilterComponent"
           component={FilterComponent}
+          options={{ headerShown: true }}
         />
         <Stack.Screen
           name="FilteredData"
           component={FilteredDataComponent}
+          options={{ headerShown: true }}
         /> */}
         <Stack.Screen
           name="Maps"
           component={Maps}
-          options={{ headerShown: false }}
+          options={{ headerShown: true }}
         />
         <Stack.Screen
           name="RequestTour"
