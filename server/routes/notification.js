43de5a07ef781e1764038f38cb4fb5606cb15fe3
unknown <shaymabahi330@gmail.com>
2024-07-23 22:11:21 +0100

    working on dashbord

--- server/routes/notification.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const notifiController = require('../controller/notification.js');
-
-router.get('/', notifiController.getAllNotifications);
-router.get('/unreadcount', notifiController.getUnreadCount);
-router.post('/markasread', notifiController.markAsRead);
-
-module.exports = router;
