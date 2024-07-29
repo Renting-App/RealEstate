aa126fcd529e04ec2fd8ebe0d0c256265a778947
ines jannedi <inesjannedi11@gmail.com>
2024-07-29 19:21:12 +0300

    ...

--- client/app/HousesScreen.tsx
+++ client/app/HousesScreen.tsx
@@ -103,7 +103,7 @@ const HousesScreen: React.FC<HousesScreenProps> = ({ route }) => {
 
   const fetchResidences = () => {
     setLoading(true);
-    fetch("http://192.168.1.105:5800/houses")
+    fetch("http://192.168.1.13:5800/houses")
       .then((response) => response.json())
       .then((data) => {
         const mappedResidences = data.map((residence: any) => ({
